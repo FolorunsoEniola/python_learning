a028c21d9b8b21b9f71000f0c61bd77918842a29
captainboye <folorunsoeniola3@gmail.com>
2025-07-26 16:13:41 +0100

    commit to git

---
+++ debugg.py
@@ -0,0 +1,17 @@
+import logging
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s -  %(levelname)s -  %(message)s')
+import random
+guess = ''
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input()
+toss = random.randint(0, 1)  # 0 is tails, 1 is heads
+if toss == guess:
+    print('You got it!')
+else:
+    print('Nope! Guess again!')
+    guess = input()
+    if toss == guess:
+        print('You got it!')
+    else:
+        print('Nope. You are really bad at this game.')
