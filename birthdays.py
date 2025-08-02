309477952d6c873c0a46a5c04b835e9af2dcd01c
captainboye <folorunsoeniola3@gmail.com>
2025-08-02 21:20:50 +0100

    add to git

--- birthdays.py
+++ birthdays.py
@@ -1,8 +1,18 @@
  # see how dictionaries work
  # and also the while loop
 # birthdays.py
-birthdays = {'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4'}
+import json
+birthdays = {'Alice': 'Apr 1', 'Bob': 'Dec 12', 'Carol': 'Mar 4', 'Bunmi': 'Apr 27'}
 
+# Load data from file if it exists
+try:
+    with open('birthdays.json', 'r') as f:
+        birthdays = json.load(f)
+except FileNotFoundError:
+    pass 
+    
+
+# Your while loop here...
 while True:
     print('Enter a name: (enter to quit)')
     name = input('name:')
@@ -17,3 +27,7 @@ while True:
         bday = input()
         birthdays[name] = bday
         print('Birthday database updated.')
+
+# Save updated data
+with open('birthdays.json', 'w') as f:
+    json.dump(birthdays, f)
---
+++ testrun.py
@@ -0,0 +1,4 @@
+def sayhi():
+    name = input('please enter your name:')
+    return (f'hello {name}, you are welcome to my world and I love you xo xo muchs')
+print(sayhi())
