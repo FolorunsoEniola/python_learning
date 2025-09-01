b83211e660459843fc0da72857232b089b385884
captainboye <folorunsoeniola3@gmail.com>
2025-09-01 20:52:13 +0100

    add .gitignore

--- bug.py
+++
@@ -1,3 +0,0 @@
-while True:
-    print("This is a bug.")# this continuously prints "This is a bug."
-    # press ctrl + c or ctrl + D to stop it
--- read_csv.py
+++
@@ -1,8 +0,0 @@
-import csv #import to read a .csv file
-from pprint import pprint     #import pprint for the output to be set and well arranged
-
-with open('portfolio.csv', 'r') as f: # open a file and set the mode
-    f_csv = csv.reader(f)             # read the file with csv.reader
-    headers = next(f_csv)             # the next command will skip the header
-    rows = list(f_csv)                # this command defines every list as a role
-    pprint(rows)                      # will arrange the output better than just print
