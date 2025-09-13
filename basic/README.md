c04f0c235159358c90466ef7ae4cc9e9219121a7
captainboye <folorunsoeniola3@gmail.com>
2025-09-13 13:43:06 +0100

    remove all files on branch

--- basic/README.md
+++
@@ -1,2 +0,0 @@
-# python_learning
-trying out python
--- basic/createzip.py
+++
@@ -1,8 +0,0 @@
-import zipfile
-with open('bacon.txt', 'w') as files:
-    files.write('Hello' * 1000,)
-    # note the zipfile and ZipFile
-    # the zipfile is the name of the module
-    # the ZipFile is the name of the function
-with zipfile.ZipFile('bacon.zip', 'w') as myzip:
-    myzip.write('bacon.txt', compress_type=zipfile.ZIP_DEFLATED)
